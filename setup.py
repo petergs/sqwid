7f22f46d7e447ec4bbe5794363ec404a603f6eb4
petergs <pstratis11@gmail.com>
2023-02-26 14:29:34 -0500

    Adjust project structure to actual package

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name="sqwid", version="0.0.1", packages=find_packages(include=["sqwid"]))
+setup(name="sqwid", version="0.0.1", packages=["sqwid"], package_dir={"": "src"})
